1abdc7a4e1b8d8a01c68a27d8f0b7d939e5c74f2
martinsgustavo <martinsgustavox@gmail.com>
2018-10-09 16:12:11 -0400

    v1.53

--- app/src/main/java/com/claudebernard/projetbf21/comm/ApiClient.java
+++ app/src/main/java/com/claudebernard/projetbf21/comm/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://10.2.10.205:8080";
+    private static final String BASE_URL = "http://35.183.168.181:80";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
